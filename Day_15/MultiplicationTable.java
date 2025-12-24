0d359313e7608b120152ab5e55ca103681003f55
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2025-12-24 22:49:02 +0530

    Add files via upload

---
+++ Day_15/MultiplicationTable.java
@@ -0,0 +1,14 @@
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        
+        int number = 7;
+
+        for(int i = 1;i <= 10; i++)
+        {
+            number = i * 8;
+            System.err.println(" " + number);
+        }
+    }
+    
+}
