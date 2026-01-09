38c6dcae251d6532cfdf8e2304134a76b5bdc938
Adi Jadhav <111346341+99adi-jadhav@users.noreply.github.com>
2026-01-09 22:52:09 +0530

    Add files via upload

---
+++ Day_22/Number_pattern_1 (1).java
@@ -0,0 +1,22 @@
+package com.java.patterns;
+
+public class Number_pattern_1 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0 ; i<= 6 ; i++ ) {
+			
+			for(int n = 1 ; n<=i ; n++) {
+				
+				System.out.print(" "+n);
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+
+	}
+
+}
---
+++ Day_22/for_loop.java
@@ -0,0 +1,15 @@
+package com.control_statements;
+
+public class for_loop {
+
+	public static void main(String[] args) {
+	
+		 for(int i=0 ; i<=50 ; i++ ) {
+			  
+			  System.out.println(i);
+			 
+		 }
+
+	}
+
+}
